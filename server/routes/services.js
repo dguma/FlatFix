9f45137296a98bb67abc53fac4e4505c2737ef0b
dguma <dguma@me.com>
2025-08-06 02:14:26 -0400

    Test: minimal services.js to isolate route parsing error

--- server/routes/services.js
+++ server/routes/services.js
@@ -22,6 +22,32 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
+// Basic test route
+router.get('/test', (req, res) => {
+  res.json({ message: 'Services route is working' });
+});
+
+module.exports = router;
+
+// Middleware to verify JWT token (same as in auth.js)
+const jwt = require('jsonwebtoken');
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token required' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
 // Create a new service request
 router.post('/request', authenticateToken, async (req, res) => {
   try {
